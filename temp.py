709be31d4eff0d01d9fb0bf6e1ed5636737f3734
navnit0707 <knavnit81@gmail.com>
2021-08-14 11:06:25 +0530

    reading image in gray and colourful

--- temp.py
+++ temp.py
@@ -1,8 +1,16 @@
 import cv2
 
-img1 = cv2.imread("Z:\\projecttemp\\Python_openCv\\data\\4.jpg",0)
+img1 = cv2.imread("Z:\\projecttemp\\Python_openCv\\data\\4.jpg",1) #1 for original
 img1 = cv2.resize(img1,(1280,700)) #width
 print(img1)
 cv2.imshow("original",img1)
+
+#cv2 to read imagein gray
+
+img2 = cv2.imread('Z:\\projecttemp\\Python_openCv\\data\\4.jpg',0) #0 for gray
+img2 = cv2.resize(img2,(1280,700)) #width , height
+cv2.imshow("gray scale image",img2)
+print("image in gray scale ==\n",img2) 
+
 cv2.waitKey()
 cv2.destroyAllWindows()
