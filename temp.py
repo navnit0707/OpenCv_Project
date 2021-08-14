6031a0ff3de78600a9360188131438542fddfa9a
navnit0707 <knavnit81@gmail.com>
2021-08-14 11:40:13 +0530

    use to read image in unchanged increase saturation

--- temp.py
+++ temp.py
@@ -12,5 +12,11 @@ img2 = cv2.resize(img2,(1280,700)) #width , height
 cv2.imshow("gray scale image",img2)
 print("image in gray scale ==\n",img2) 
 
+#cv3 unchanged use -1 instead of 0 and 1 increase saturation
+img2 = cv2.imread('Z:\\projecttemp\\Python_openCv\\data\\4.jpg',-1) #0 for gray
+img2 = cv2.resize(img2,(1280,700)) #width , height
+cv2.imshow("unchanged image",img2)
+print("image unchanged scale ==\n",img2) 
+
 cv2.waitKey(0)  #0 for static cut the output
 cv2.destroyAllWindows()
